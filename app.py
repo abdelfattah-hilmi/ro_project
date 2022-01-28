a5cdce8ed49df97d6e9dca3ebd7763c6074c611c
Abdelfattah <hilmiabdelfattah@gmail.com>
2022-01-28 19:26:14 +0100

    initial data manipulation

--- app.py
+++ app.py
@@ -1,39 +1,113 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
-from dash import dcc
+import dash_cytoscape as cyto
 from dash import html
-import plotly.express as px
-import pandas as pd
+import cylo_formatter as hilmi
+
+my_dataset = "test.mtx"
+
+GRAPH = hilmi.create_elements(dataset=my_dataset)
 
 app = dash.Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+app.layout = html.Div([
+    cyto.Cytoscape(
+        id='cytoscape-two-nodes',
+        layout={'name': 'circle'},
+        style={'width': '100%', 'height': '600px','background': 'linear-gradient(90deg, rgba(241,188,162,1) 0%, rgba(244,255,128,0.8379945728291316) 46%, rgba(255,89,193,0.7539609593837535) 100%)'},
+        elements=GRAPH
+        
+        # elements=[
+        #     #---------------------nodes aka vertecies--------------------
+        #     {'data': {'id': 'one', 'label': 'Node 1'}, },
+        #     {'data': {'id': 'two', 'label': 'Node 2'}, },
+        #     {'data': {'id': 'three', 'label': 'Node 3'}, },
+        #     {'data': {'id': 'four', 'label': 'Node 4'}, },
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(
-    children = 
-    [
-
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
+        #     #---------------------edges aka edges hhh--------------------
+        #     {'data': {'source': 'one', 'target': 'two'}},
+        #     {'data': {'source': 'one', 'target': 'three'}},
+        #     {'data': {'source': 'two', 'target': 'three'}},
+        #     {'data': {'source': 'three', 'target': 'four'}},
+            
+        # ]
     )
 ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# dash starting template
+
+
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+# import dash
+# from dash import dcc
+# from dash import html
+# import plotly.express as px
+# import pandas as pd
+
+# app = dash.Dash(__name__)
+
+# # assume you have a "long-form" data frame
+# # see https://plotly.com/python/px-arguments/ for more options
+# df = pd.DataFrame({
+#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#     "Amount": [4, 1, 2, 2, 4, 5],
+#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+# })
+
+# fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+# app.layout = html.Div(
+#     children = 
+#     [
+
+#     html.H1(children='Hello Dash'),
+
+#     html.Div(children='''
+#         Dash: A web application framework for your data.
+#     '''),
+
+#     dcc.Graph(
+#         id='example-graph',
+#         figure=fig
+#     )
+# ])
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
---
+++ cylo_formatter.py
@@ -0,0 +1,50 @@
+'''
+this script takes the raw data from the dataset and formats its fron dash cytoscape element:
+the final data format should look like this :
+elements=[
+            #---------------------nodes aka vertecies--------------------
+            {'data': {'id': 'one', 'label': 'Node 1'}, },
+            {'data': {'id': 'two', 'label': 'Node 2'}, },
+            {'data': {'id': 'three', 'label': 'Node 3'}, },
+            {'data': {'id': 'four', 'label': 'Node 4'}, },
+
+            #---------------------edges aka edges hhh--------------------
+            {'data': {'source': 'one', 'target': 'two'}},
+            {'data': {'source': 'one', 'target': 'three'}},
+            {'data': {'source': 'two', 'target': 'three'}},
+            {'data': {'source': 'three', 'target': 'four'}},
+            
+        ]
+'''
+
+
+def get_data_from(filepath:str) -> list :
+    '''
+    get dataset raw lines 
+    '''
+
+    with open(filepath) as file:
+        lista = file.readlines()
+        return lista
+
+
+def create_elements(dataset:str) -> list:
+    data = get_data_from( dataset )
+    data = [ (s[:-1]).split() for s in data ]
+
+    # unzip the data list i.e: if data = [(a,b),(c,s)] then source = (a,c) and dest = (b,d)
+    source_nodes, destination_nodes = zip( *data ) 
+
+    nodes = [
+        {
+            'data':{'id':f'{i}','label': f'prod {i}' } 
+            }for i in source_nodes + destination_nodes
+    ]
+
+    edges = [
+        {
+            'data':{'source':f'{i}','target': f'{j}'}   
+            }for i,j in data
+    ]
+
+    return nodes+edges
